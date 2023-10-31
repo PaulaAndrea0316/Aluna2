5b03c990f71e1a3bdca262f1e697199acd1a6d9e
Paula Andrea Gonzalez H <paulaandrea0316@yahoo.com.mx>
2023-10-30 23:01:51 -0500

    cambios footer
    cambios footer

--- front/src/components/layout/Footer.js
+++ front/src/components/layout/Footer.js
@@ -1,14 +1,13 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
 export const Footer = () => {
-    return (
-      <Fragment>
-          <footer className="py-1">
-              <p className="text-center mt-1">
-                  Aluna Store - 2023 -Todos los derechos reservados
-              </p>
-          </footer>
-  
-      </Fragment>
-    )
-  }
+  return (
+    <Fragment>
+      <footer className="py-1">
+        <p className="text-center mt-1">
+          Aluna Store - 2023 -Todos los derechos reservados
+        </p>
+      </footer>
+    </Fragment>
+  );
+};
